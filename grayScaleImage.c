26a521b86277a421622e6836834f91f25e4604f2
aichagc <aichagacem115@.com>
2024-11-20 19:00:34 +0100

    correction

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -1,12 +1,28 @@
 #include "config/grayScaleImage.h"
+
 // Parameters:
 //   w: width of the image
 //   h: height of the image
+
 // Return value
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
-   // YOUR CODE HERE - BEGIN
-
-   // YOUR CODE HERE - END
-   return 0;
+    // YOUR CODE HERE - BEGIN
+    float size_bitmap = w * h; 
+    // the rule is h*w*8 but since we are looking for the Bytes value, 
+    // then the 8 is removed since we were about to divide in 8 anyway 
+    return size_bitmap; 
+    // YOUR CODE HERE - END
 }
+
+/* 
+int main (){ 
+    int w, h; 
+    printf("enter w \n"); 
+    scanf("%d", &w); 
+    printf("enter h \n"); 
+    scanf("%d", &h); 
+    printf("The result in Bytes is %f", grayScaleImage(w, h)); 
+} 
+*/
+
--- video.c
+++ video.c
@@ -1,16 +1,45 @@
 #include <string.h>
+#include <stdio.h>
 #include "config/video.h"
+
 // Parameters:
 //   w: width of the image
 //   h: height of the image
-//   durationMovie: duration in second of movie (colored image)
-//   durationCredits: duration in second of credit (image Black/White)
-//   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
-// Return value
-//   colored video size (based on the unit passed parametter)
-float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
-   // YOUR CODE HERE - BEGIN
+//   durationMovie: duration in seconds of the movie (colored image)
+//   durationCredits: duration in seconds of credits (black/white image)
+//   unit: Unit of the output value. It could be 'bt' for bytes, 'ko' for kilobits, 'mo' for megabits, 'go' for gigabits
 
-   // YOUR CODE HERE - END
-   return 0;
+// Return value
+//   colored video size (based on the unit passed parameter)
+float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
+    // Bits per pixel for colored image (24 bits)
+    float bitsPerPixel = 24;
+
+    // Calcul des tailles pour l'image en couleur et en noir et blanc
+    float clrImage = w * h * bitsPerPixel * durationMovie * fps;
+    float BImage = w * h * durationCredits * fps;
+
+    // Taille totale en bits
+    float sizeInBits = clrImage + BImage;
+
+    // Taille dans l'unité souhaitée
+    float size;
+
+    // Convertir la taille en fonction de l'unité demandée
+    if (strcmp(unit, "bt") == 0) {
+        size = sizeInBits; // Taille en bits
+    } else if (strcmp(unit, "ko") == 0) {
+        size = sizeInBits / 1024; // Taille en kilobits
+    } else if (strcmp(unit, "mo") == 0) {
+        size = sizeInBits / (1024 * 1024); // Taille en mégabits
+    } else if (strcmp(unit, "go") == 0) {
+        size = sizeInBits / (1024 * 1024 * 1024); // Taille en gigabits
+    } else {
+        // Si l'unité n'est pas reconnue, renvoyer -1 ou une valeur d'erreur
+        return -1.0f;
+    }
+
+    // Retourner la taille en octets
+    return size / 8;
 }
+
